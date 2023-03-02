b654d77bd6efda8d961a04853923ad6247ec4f59
YehwanJeon <junyehan@gmail.com>
2023-03-02 23:39:27 +0900

    init

---
+++ src/main/java/com/example/homeworkcrawler/HomeworkCrawlerApplication.java
@@ -0,0 +1,12 @@
+package com.example.homeworkcrawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkCrawlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeworkCrawlerApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/homeworkcrawler/HomeworkCrawlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homeworkcrawler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkCrawlerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
